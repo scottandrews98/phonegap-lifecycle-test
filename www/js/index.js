c364e3c03f11bdde622b922d78ddcb93e7f98996
Scott Andrews <ANDS3_16@UNI.WORC.AC.UK>
2017-10-12 14:09:15 +0100

    fourth

--- www/js/index.js
+++ www/js/index.js
@@ -8,7 +8,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 	
 function updateDisplay() {
     document.getElementById("launched").innerHTML = "Application launched" + launched_count;
-    document.getElementById("resumed").innerHTML = "Application paused" + paused_count_count;
+    document.getElementById("resumed").innerHTML = "Application paused" + paused_count;
     document.getElementById("passed").innerHTML = "Application resumed" + resumed_count;
 }
 
