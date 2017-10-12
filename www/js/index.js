1c4eeda0ff38304e16d83190f0ae46b179728674
Scott Andrews <ANDS3_16@UNI.WORC.AC.UK>
2017-10-12 13:58:01 +0100

    Changed from jquery mobile to javascript
    Javscript change

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,7 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+// Adds device ready function and gives it the name onDeviceReady
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
@@ -9,13 +10,17 @@ function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
 	$("#paused").text("Application resumed: " + resumed_count);
+    
+    document.getElementById("launched").innerHTML = "Application launched" + launched_count;
+    document.getElementById("resumed").innerHTML = "Application paused" + paused_count_count;
+    document.getElementById("passed").innerHTML = "Application resumed" + resumed_count;
 }
 
 
 // device APIs are available
 //
     function onDeviceReady() {
-	
+	// When the device has fully loaded run the function.
 	document.addEventListener("resume", onResume, false);
 	document.addEventListener("pause", onPause, false);
 	
@@ -27,18 +32,22 @@ function updateDisplay() {
 
 
     function onPause() {
-	
+	// When the app is paused so the user exits out of the app this function runs.
+    // Adds 1 to the variable paused_count    
 	paused_count++;
+    // Runs the updateDisplay function    
 	updateDisplay();
-	    
+    // Will run an alert on the phone which will display the text pause. 
 	alert("pause");
     }
 	
 
     function onResume() {
-		
+    // When the user navigates back to the app this is ran.
+    // Adds 1 to the variable resumed_count     
 	resumed_count++;
+    // Runs the updateDisplay function    
 	updateDisplay();
-	    
+	// Will run an alert on the phone which will display the text resume.    
 	alert("resume");
     }
